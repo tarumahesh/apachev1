9800920209695e6447c63ad1f602087076a25378
tarumahesh <34134599+tarumahesh@users.noreply.github.com>
2020-12-30 10:46:17 +0530

    Update main.js

--- main.js
+++ main.js
@@ -7,6 +7,6 @@ var server = http.createServer(function (request, response) {
   response.end("Hello Get into DevOps\n");
 });
 
-// listen on localhost:8000
-server.listen(8000);
-console.log("Server listening at http://127.0.0.1:8000/");
+// listen on localhost:80
+server.listen(80);
+console.log("Server listening at http://127.0.0.1:80/");
